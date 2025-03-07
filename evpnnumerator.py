0f924fd61b6e03a130b8b87e743194a89ce42830
yuke1922 <yuke1922@lmc.lmc>
2025-03-07 07:36:30 -0600

    added local tools

--- evpnnumerator.py
+++ evpnnumerator.py
@@ -1,4 +1,5 @@
 from ipaddress import IPv4Address, IPv4Network, IPv4Interface
+import lmtools
 
 class Fabric:
     def __init__(self):
---
+++ lmtools.py
@@ -0,0 +1,16 @@
+import os
+from pprint import pprint
+from icecream import ic
+
+def cls():
+    os.system("clear")
+
+def pt(var):
+    print(type(var))
+
+def p(var):
+    print(var)
+
+def pp(var):
+    pprint(var)
+
